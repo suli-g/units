3252e0bcf16f8f407a42658d9ddbc6c802040335
Sulaiman Gafoor <5022721+suli-g@users.noreply.github.com>
2024-11-26 17:26:41 +0200

    Create script for generating docs and moving them to the 'docs' directory

---
+++ doc_utils/constants.py
@@ -0,0 +1,15 @@
+from .functions import build_docs_and_move
+
+ABBREVIATIONS = {
+    "b": "build",
+}
+
+COMMANDS = {
+    "build": "Builds the documentation and moves it to the 'docs' folder",
+}
+ACTIONS = {"build": build_docs_and_move}
+
+COMMAND_HELP = "\n".join(
+    f"-{short_name}, --{full_name} - {COMMANDS[full_name]}"
+    for short_name, full_name in ABBREVIATIONS.items()
+)
